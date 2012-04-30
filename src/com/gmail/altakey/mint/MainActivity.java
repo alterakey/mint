961931b055a3d2a4f6c18fe78d68a1f434fab19b
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2012-04-30 23:53:00 +0900

    Modelling main list screen

--- src/com/gmail/altakey/mint/MainActivity.java
+++ src/com/gmail/altakey/mint/MainActivity.java
@@ -1,15 +1,19 @@
 package com.gmail.altakey.mint;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.*;
 
-public class MainActivity extends Activity
+public class MainActivity extends ListActivity
 {
+    private final String[] items = {"item 1", "item 2"};
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.list_item_title, items));
     }
 }
