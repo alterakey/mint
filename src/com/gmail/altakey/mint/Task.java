254ea5ae076873e7a0f17c6da327c07fcf0d81d5
Takahiro Yoshimura <taky@cs.monolithworks.co.jp>
2013-01-06 12:55:33 +0900

    Holding cookie to model

--- src/com/gmail/altakey/mint/Task.java
+++ src/com/gmail/altakey/mint/Task.java
@@ -11,6 +11,7 @@ import java.util.Date;
 
 public class Task {
     public long _id;
+    public String _cookie;
     public long id;
     public String title;
     public String note;
@@ -39,21 +40,34 @@ public class Task {
         completed = at / 1000;
     }
 
+    public void addCookie() {
+        note = String.format("%s\n(mint:%s)", note, DB.nextCookie());
+    }
+
+    public void removeCookie() {
+        note = note.replaceAll("\n?(mint:[0-9a-f]{32,})", "");
+    }
+
+    public void isReplica() {
+        return note.contains(String.format("(mint:%s)", _cookie));
+    }
+
     public static Task fromCursor(Cursor c, int offset) {
         final Task task = new Task();
         task._id = c.getLong(0 + offset);
-        task.id = c.getLong(1 + offset);
-        task.title = c.getString(2 + offset);
-        task.note = c.getString(3 + offset);
-        task.modified = c.getLong(4 + offset);
-        task.completed = c.getLong(5 + offset);
-        task.folder = c.getLong(6 + offset);
-        task.context = c.getLong(7 + offset);
-        task.priority = c.getLong(8 + offset);
-        task.star = c.getLong(9 + offset);
-        task.duedate = c.getLong(10 + offset);
-        task.duetime = c.getLong(11 + offset);
-        task.status = c.getString(12 + offset);
+        task._cookie = c.getString(1 + offset);
+        task.id = c.getLong(2 + offset);
+        task.title = c.getString(3 + offset);
+        task.note = c.getString(4 + offset);
+        task.modified = c.getLong(5 + offset);
+        task.completed = c.getLong(6 + offset);
+        task.folder = c.getLong(7 + offset);
+        task.context = c.getLong(8 + offset);
+        task.priority = c.getLong(9 + offset);
+        task.star = c.getLong(10 + offset);
+        task.duedate = c.getLong(11 + offset);
+        task.duetime = c.getLong(12 + offset);
+        task.status = c.getString(13 + offset);
         return task;
     }
 
