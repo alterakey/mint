3ef3f7f20914db019833a2ff9cf03714c7845d4d
Takahiro Yoshimura <altakey@gmail.com>
2013-10-12 02:49:10 +0900

    Cosmetic fixes

--- src/com/gmail/altakey/mint/timer/MainActivity.java
+++ src/com/gmail/altakey/mint/timer/MainActivity.java
@@ -2,11 +2,13 @@ package com.gmail.altakey.mint.timer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
 
         getFragmentManager()
